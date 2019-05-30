0045921b7f0a164b7637893ec3c645f8ddc4d62b
Francisco Guajardo <francisco.guajardo.v@usach.cl>
2019-05-30 11:28:40 -0400

    Avance

--- programa.c
+++ programa.c
@@ -17,7 +17,9 @@ int main( int argc, char* argv[] )
 
     else if ( argc == 3 && strcmp ( argv[1], "1" ) )
     {
-        //argv[2] = nombre archivo.
+        Matriz * matriz;
+
+        matriz = leerArchivo ( argv[2] );
     }
 
     else if ( argc == 1 )
@@ -31,4 +33,93 @@ int main( int argc, char* argv[] )
     }
 
     printf ( "\n\n************************** Fin Programa *************************\n\n" );
+}
+
+Matriz * leerArchivo ( char fileName[] )
+{
+    int vertices = 0;
+    int aristas = 0;
+    int pesos = 0;
+    Matriz * matriz = crearMatriz(); 
+    FILE * archivo = fopen( fileName, "r" );
+
+    if ( archivo == NULL)
+    {
+        perror ( "Error al intentar de abrir el archivo... " );
+        printf ( "\n\n************************** Fin Programa *************************\n\n" );
+        exit ( EXIT_FAILURE );
+    }
+
+    fclose(archivo);
+
+    matriz -> verticesTotales = vertices;
+    matriz -> aristasTotales = aristas;
+    matriz -> pesosTotal = pesos;
+    return matriz;
+}
+
+Matriz * crearMatriz()
+{
+    // Se pide el espacio de memoria para la estructura.
+	Matriz * matriz = ( Matriz * ) malloc ( sizeof ( Matriz ) );
+
+	// Si la asignacion de memoria es exitosa, se inicializan las variables.
+	if ( matriz != NULL )
+    {
+	    matriz -> verticesTotales = 0;
+        matriz -> aristasTotales = 0;
+        matriz -> pesosTotal = 0;
+        matriz -> matrix = NULL;
+        return matriz;
+	}
+
+	// En caso contrario se retorna nulo.
+	else
+    {
+		return NULL;
+	}
+}
+
+Lista * crearLista()
+{
+    // Se pide el espacio de memoria para la estructura.
+	Lista * lista = ( Lista * ) malloc ( sizeof ( Lista ) );
+
+    // Si la asignacion de memoria es exitosa, se inicializan las variables.
+	if ( lista != NULL )
+    {
+        lista -> vertice = 0;
+	    lista -> listaAdyacencia = NULL;
+		lista -> sgte = NULL;
+        lista -> ant = NULL;
+		return lista;
+	}
+
+	// En caso contrario se retorna nulo.
+	else
+    {
+		return NULL;
+	}
+
+}
+
+Nodo * crearNodo()
+{
+    // Se pide el espacio de memoria para la estructura.
+	Nodo * nodo = ( Nodo * ) malloc ( sizeof ( Nodo ) );
+
+    // Si la asignacion de memoria es exitosa, se inicializan las variables.
+	if ( nodo != NULL )
+    {
+        nodo -> vertice = 0;
+	    nodo -> peso = 0;
+		nodo -> sig = NULL;
+		return nodo;
+	}
+
+	// En caso contrario se retorna nulo.
+	else
+    {
+		return NULL;
+	}
 }
--- programa.h
+++ programa.h
@@ -2,5 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 
-//TDA
+/********** Estructuras **********/
+typedef struct Nodo
+{
+    int vertice;
+    int peso;
+    struct Nodo *sig;
+} Nodo;
 
+typedef struct Lista
+{
+    int vertice;
+    Nodo *listaAdyacencia;
+    struct Lista *sgte;
+    struct Lista *ant;
+} Lista;
+
+typedef struct 
+{
+    int verticesTotales;
+    int aristasTotales;
+    int pesosTotal;
+    Lista *matrix;
+} Matriz;
+
+/********** Prototipos **********/
+Matriz * leerArchivo ( char fileName[] );
+Matriz * crearMatriz();
+Lista * crearLista();
+Nodo * crearNodo();
